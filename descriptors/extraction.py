92cc9d0f06b325c85187c2e4a002fb8d9ec22cea
Paweł <kalicki.paul@icloud.com>
2021-01-14 12:19:38 +0100

    :sparkles: Added split for paths

--- descriptors/extraction.py
+++ descriptors/extraction.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import shutil
 import numpy as np
 from src.utils import get_project_root
 from skimage import io, img_as_ubyte
@@ -35,6 +36,7 @@ class FeatureClass:
         self.images = [io.imread(p) for p in self.img_paths]
 
     def preprocessing(self, output_dir_name):
+        paths = []
         for i, img in enumerate(self.images):
             output_dir = os.path.join(base_path, output_dir_name)
             if not os.path.isdir(output_dir):
@@ -58,18 +60,23 @@ class FeatureClass:
                 binary = img2 < thresh
                 io.imsave(path_img_target, img_as_ubyte(binary))
                 print(f"{path_img_target} ---- DONE")
+            paths.append([os.path.join(output_dir_name, self.class_name), self.img_names[i]])
+        return paths
 
 
 class FeatureData:
     class_names = []
+    bin_images = []
     is_bin = False
 
     def __init__(self, input_dir):
+        self.bin_path = None
+        self.bin_images_paths = {}
+        self.split_path = None
         self.classes = []
         self.child_nbr = None
         self.input_dir = input_dir
         self.load_data()
-        self.split_test_train()
 
     def __str__(self):
         return f"Number of classes for extraction: {self.child_nbr}"
@@ -98,26 +105,100 @@ class FeatureData:
         print('Binarize process ---- START')
         for class_name in self.classes:
             class_name.load_images()
-            class_name.preprocessing(output_dir_name)
+            paths = class_name.preprocessing(output_dir_name)
+            self.bin_images_paths[class_name.class_name] = paths
         self.is_bin = True
+        self.bin_path = output_dir_name
         print('Binarize process ---- DONE')
-
-    def get_bin(self):
-        pass
-        # img_bin = [io.imread(p) for p in self.img_paths]
 
     def is_binarized(self):
         return self.is_bin
 
-    def split_test_train(self):
-        if self.is_binarized():
-            path = os.path.join(root, base_path, 'training')
-            img_train_paths, img_test_paths = [], []
+    def load_bin_images(self):
+        self.bin_images = [io.imread(p) for p in self.bin_images_paths]
 
-            print('Split data process ---- START')
-            print(path)
-            print('Split data process ---- DONE')
+    def split_paths(self, input, output, step):
+        test_dir = os.path.join(base_path, output, 'TEST')
+        train_dir = os.path.join(base_path, output, 'TRAIN')
+        test_paths, train_paths = [], []
 
-            return path, img_train_paths, img_test_paths
+        if not os.path.isdir(test_dir):
+            print(f"Directory {test_dir} not found.")
+            os.mkdir(test_dir)
+            print("Directory '%s' created" % test_dir)
+        if not os.path.isdir(train_dir):
+            print(f"Directory {train_dir} not found.")
+            os.mkdir(train_dir)
+            print("Directory '%s' created" % train_dir)
+
+        for item in self.classes:
+            test_class_path = os.path.join(test_dir, item.class_name)
+            train_class_path = os.path.join(train_dir, item.class_name)
+
+            if not os.path.isdir(test_class_path):
+                print(f"Directory {test_class_path} not found.")
+                os.mkdir(test_class_path)
+                print("Directory '%s' created" % test_class_path)
+
+            if not os.path.isdir(train_class_path):
+                print(f"Directory {train_class_path} not found.")
+                os.mkdir(train_class_path)
+                print("Directory '%s' created" % train_class_path)
+
+            if self.is_binarized():
+                for bin_dir in self.bin_images_paths:
+                    for i, img_file in enumerate(self.bin_images_paths[bin_dir]):
+                        dest_folder = os.path.join(
+                            train_dir,
+                            bin_dir
+                        ) if i == 0 else os.path.join(
+                            test_dir,
+                            bin_dir
+                        )
+
+                        if os.path.isdir(dest_folder):
+                            path_to_moved = os.path.join(dest_folder, img_file[1])
+                            if i == 0:
+                                train_paths.append(path_to_moved)
+                            else:
+                                test_paths.append(path_to_moved)
+
+                            if not os.path.isfile(path_to_moved):
+                                shutil.copy(os.path.join(root, base_path, *img_file), dest_folder)
+                                print(f"File {img_file[1]} moved.")
+                                print(f"From: {os.path.join(root, base_path, img_file[0])}")
+                                print(f"To: {dest_folder}")
+
+        return (test_dir, train_dir), (train_paths, test_paths)
+
+    def split_test_train(self, output_dir_name, bin=False, step=0):
+        input_path = ""
+        self.split_path = os.path.join(
+            root,
+            base_path,
+            f"bin_{output_dir_name}"
+        ) if bin else os.path.join(
+            root,
+            base_path,
+            output_dir_name
+        )
+
+        if not os.path.isdir(self.split_path):
+            print(f"Directory {self.split_path} not found.")
+            os.mkdir(self.split_path)
+            print("Directory '%s' created" % self.split_path)
+
+        if bin:
+            if self.is_binarized():
+                input_path = os.path.join(root, base_path, self.bin_path)
+            else:
+                print("Make binarization process!")
         else:
-            print("Make binarization process!")
+            input_path = os.path.join(root, base_path, self.input_dir)
+
+        print('Split data process ---- START')
+        paths, images = self.split_paths(input_path, self.split_path, step)
+        img_train_paths, img_test_paths = images
+        print('Split data process ---- DONE')
+
+        return img_train_paths, img_test_paths
